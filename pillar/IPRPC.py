9bd43cda102d41aad449218c622cd60ecf7994c6
graysonhead <grayson@graysonhead.net>
2020-12-20 23:21:17 -0600

    Adding IPRPCMessage and IPRPCMessageType classes

--- pillar/IPRPC.py
+++ pillar/IPRPC.py
@@ -3,6 +3,10 @@ from .exceptions import IPRPCException
 
 
 class IPRPCCall:
+    """
+    This class represents an IPRPC (InterPlanetary Remote Procedure Call) call. It will be wrapped in a message before
+    sending to an IPFS Pubsub queue.
+    """
     attributes = {}
 
     def __init__(self, **kwargs):
@@ -27,10 +31,13 @@ class IPRPCCall:
             setattr(self, arg, value)
 
     def serialize_to_json(self):
+        return json.dumps(self.serialize_to_dict())
+
+    def serialize_to_dict(self):
         return_dict = {"message_type": self.message_type}
         for attr, value in self.attributes.items():
             return_dict.update({attr: getattr(self, attr)})
-        return json.dumps(return_dict)
+        return return_dict
 
 
 class IPRPCRegistry:
@@ -44,9 +51,88 @@ class IPRPCRegistry:
     @classmethod
     def deserialize_from_json(cls, serialized_call: str):
         rpc_dict = json.loads(serialized_call)
-        class_name = rpc_dict.get('message_type')
+        return IPRPCRegistry.deserialize_from_dict(rpc_dict)
+
+    @classmethod
+    def deserialize_from_dict(cls, serialized_call: dict):
+        class_name = serialized_call.get('message_type')
         target_class = cls.message_types.get(class_name)
-        return target_class(**rpc_dict)
+        return target_class(**serialized_call)
+
+
+class IPRPCMessageType:
+    INLINE = 1
+    INLINE_ENCRYPTED = 2
+    CID = 3
+    CID_ENCRYPTED = 4
+
+
+class IPRPCMessage:
+
+    def __init__(self,
+                 msg_type: IPRPCMessageType,
+                 dst_peer: str = '',
+                 broadcast: bool = False,
+                 msg_cid: str = '',
+                 call: IPRPCCall = None
+                 ):
+
+        self.msg_type = msg_type
+        if dst_peer:
+            self.dst_peer = dst_peer
+        else:
+            self.dst_peer = None
+        if msg_cid:
+            self.msg_cid = msg_cid
+        else:
+            self.msg_cid = None
+        self.broadcast = broadcast
+        if call:
+            self.call = call
+        else:
+            self.call = None
+        self._validate()
+
+    def _validate(self):
+        if self.broadcast and self.dst_peer:
+            raise IPRPCException("Invalid message, cannot have broadcast arg set with dst_peer arg present")
+        if not self.broadcast and not self.dst_peer:
+            raise IPRPCException("Invalid message, broadcast False but missing dst_peer arg")
+        if self.msg_type == IPRPCMessageType.INLINE or self.msg_type == IPRPCMessageType.INLINE_ENCRYPTED:
+            if self.msg_cid:
+                raise IPRPCException("Invalid message, cannot be type inline with msg_cid arg")
+            if not self.call:
+                raise IPRPCException("Invalid message, INLINE type must have call arg")
+        if self.msg_type == IPRPCMessageType.CID or self.msg_type == IPRPCMessageType.CID_ENCRYPTED:
+            if self.call:
+                raise IPRPCException("Invalid message, cannot be type CID with call arg")
+            if not self.msg_cid:
+                raise IPRPCException("Invalid message, CID type must have msg_cid arg")
+
+    def serialize_to_json(self):
+        if self.msg_type == IPRPCMessageType.INLINE:
+            return self._dumps_inline_unencrypted()
+
+    def _dumps_inline_unencrypted(self):
+        result_dict = {'msg_type': self.msg_type,
+                       'broadcast': self.broadcast,
+                       'call': self.call.serialize_to_dict()}
+        if self.msg_cid:
+            result_dict.update({"msg_cid": self.msg_cid})
+        if self.broadcast:
+            result_dict.update({"broadcast": True})
+        else:
+            result_dict.update({"dst_peer": self.dst_peer})
+        return json.dumps(result_dict)
+
+    @staticmethod
+    def deserialize_from_json(json_string: str):
+        pass
+        message_dict = json.loads(json_string)
+        if message_dict.get("call", None):
+            call_instance = IPRPCRegistry.deserialize_from_dict(message_dict.get('call'))
+            message_dict.update({'call': call_instance})
+        return IPRPCMessage(**message_dict)
 
 
 class PingType:
--- pillar/tests/test_iprpc.py
+++ pillar/tests/test_iprpc.py
@@ -1,5 +1,10 @@
 from unittest import TestCase
-from ..IPRPC import IPRPCCall, IPRPCRegistry, PingRPC, PingType
+from ..IPRPC import IPRPCCall, \
+    IPRPCRegistry, \
+    PingRPC, \
+    PingType, \
+    IPRPCMessage, \
+    IPRPCMessageType
 from ..exceptions import IPRPCException
 
 
@@ -70,3 +75,113 @@ class TestPingRPCCall(TestCase):
     def test_create_ping_call(self):
         ping_instance = PingRPC(ping_type=PingType.REQUEST)
         self.assertEqual(PingRPC, type(ping_instance))
+
+
+class TestIPRPCMessage(TestCase):
+
+    def test_iprpc_message_inline_broadcast(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        message = IPRPCMessage(
+            IPRPCMessageType.INLINE,
+            broadcast=True,
+            call=call_instance,
+        )
+        self.assertEqual(1, message.msg_type)
+        self.assertEqual(True, message.broadcast)
+        self.assertEqual(call_instance, message.call)
+
+    def test_iprpc_message_cid_broadcast(self):
+        message = IPRPCMessage(
+            IPRPCMessageType.CID,
+            broadcast=True,
+            msg_cid="<msg_cid>"
+        )
+        self.assertEqual(IPRPCMessageType.CID, message.msg_type)
+        self.assertEqual(True, message.broadcast)
+
+    def test_iprpc_message_inline_unencrypted_serialize(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        message = IPRPCMessage(
+            IPRPCMessageType.INLINE,
+            call=call_instance,
+            dst_peer="<peer_id>",
+        )
+        raw_message = message.serialize_to_json()
+        expected_result = '{"msg_type": 1, "broadcast": false, "call": {"message_type": "PingRPC", "ping_type": 1}, "dst_peer": "<peer_id>"}'
+        self.assertEqual(expected_result, raw_message)
+
+    def test_iprpc_message_inline_encrypted_deserialize(self):
+        json_string = '{"msg_type": 1, "broadcast": false, "call": {"message_type": "PingRPC", "ping_type": 1}, "dst_peer": "<peer_id>"}'
+        message = IPRPCMessage.deserialize_from_json(json_string)
+        self.assertEqual(IPRPCMessageType.INLINE, message.msg_type)
+        self.assertEqual(False, message.broadcast)
+        self.assertEqual(PingRPC, type(message.call))
+        self.assertEqual(IPRPCMessage, type(message))
+
+
+class TestIPRPCMessageValidation(TestCase):
+
+    def test_iprpc_message_invalid_broadcast_and_dst_peer(self):
+        with self.assertRaises(IPRPCException):
+            call_instance = PingRPC(ping_type=PingType.REQUEST)
+            message = IPRPCMessage(
+                IPRPCMessageType.INLINE,
+                dst_peer="<peer_id>",
+                broadcast=True,
+                call=call_instance
+            )
+
+    def test_iprpc_message_invalid_inline_with_cid(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        with self.assertRaises(IPRPCException):
+            message = IPRPCMessage(
+                IPRPCMessageType.INLINE,
+                dst_peer="<peer_id>",
+                call=call_instance,
+                msg_cid="<cid_id>"
+            )
+
+    def test_iprpc_message_invalid_cid_with_inline(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        with self.assertRaises(IPRPCException):
+            message = IPRPCMessage(
+                IPRPCMessageType.CID,
+                dst_peer="<peer_id>",
+                call=call_instance,
+                msg_cid="<cid_id>"
+            )
+
+    def test_iprpc_message_invalid_cid_missing_arg(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        with self.assertRaises(IPRPCException):
+            message = IPRPCMessage(
+                IPRPCMessageType.CID,
+                dst_peer="<peer_id>",
+            )
+
+    def test_iprpc_message_invalid_inline_missing_arg(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        with self.assertRaises(IPRPCException):
+            message = IPRPCMessage(
+                IPRPCMessageType.INLINE,
+                dst_peer="<peer_id>",
+            )
+
+    def test_iprpc_message_invalid_broadcast_with_peer(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        with self.assertRaises(IPRPCException):
+            message = IPRPCMessage(
+                IPRPCMessageType.CID,
+                dst_peer="<peer_id>",
+                broadcast=True,
+                msg_cid="<cid_id>"
+            )
+
+    def test_iprpc_message_invalid_broadcast_false_without_peer(self):
+        call_instance = PingRPC(ping_type=PingType.REQUEST)
+        with self.assertRaises(IPRPCException):
+            message = IPRPCMessage(
+                IPRPCMessageType.CID,
+                broadcast=False,
+                msg_cid="<cid_id>"
+            )
