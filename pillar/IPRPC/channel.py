f4c50d1c06c6e13ff2544919f0396a34c699345a
graysonhead <grayson@graysonhead.net>
2020-12-23 00:54:12 -0600

    Testing Different Kind of Encoding for Messages

--- pillar/IPRPC/channel.py
+++ pillar/IPRPC/channel.py
@@ -1,7 +1,8 @@
 import aioipfs
-from .messages import IPRPCMessage
+from .messages import IPRPCMessage, IPRPCRegistry
 from ..exceptions import IPRPCMessageException
 import logging
+import base64
 
 
 class Channel:
@@ -17,19 +18,28 @@ class Channel:
         """send message in pubsub channel"""
         serialized_message = message.serialize_to_json()
         self.logger.info(f"Sending message: {serialized_message}")
+        message_bytes = base64.b64encode(serialized_message.encode())
+        print(message_bytes)
         async with self.ipfs as cli:
-            await cli.pubsub.pub(self.queue_id, serialized_message)
+            await cli.pubsub.pub(self.queue_id,
+                                 message_bytes.decode())
 
     async def get_messages(self):
         """gets messages from pubsub channel"""
         messages = []
         async with self.ipfs as cli:
             async for raw_message in cli.pubsub.sub(self.queue_id):
+                raw_message_string = raw_message.get('data').decode('utf-8')
+                serialized_data = base64.b64decode(raw_message_string).decode()
                 try:
-                    message = raw_message.deserialize_from_json()
+                    message = IPRPCMessage.deserialize_from_json(
+                        serialized_data)
                 except IPRPCMessageException as e:
                     self.logger.warning(f"Invalid message received: {e}")
                 messages.append(message)
+                self.logger.info(f"Received message from peer "
+                                 f"{raw_message.get('from').decode()}"
+                                 f": {message}")
             self.messages = messages
 
     def __repr__(self):
