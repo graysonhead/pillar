e6cc1b29c98be88e88fef7445aa1cf38cb42acac
graysonhead <grayson@graysonhead.net>
2020-12-31 02:27:08 -0600

    Created IPRPCChannel Process and Tests

--- pillar/IPRPC/channel.py
+++ pillar/IPRPC/channel.py
@@ -8,10 +8,136 @@ from .messages import IPRPCMessage, \
     PeeringHelloResponse, \
     IPRPCRegistry, \
     PeeringKeepalive
-# from queue import Queue
-# from threading import Thread
 from multiprocessing import Process, Pipe
 import time
+
+
+class PeeringStatus(Enum):
+    IDLE = 1
+    ESTABLISHING = 2
+    ESTABLISHED = 3
+
+
+class IPRPCChannel(Process):
+
+    def __init__(self,
+                 id: str,
+                 ipfs_queue_id: str,
+                 aioipfs_instance: aioipfs.AsyncIPFS,
+                 keepalive_send_interval: int = 30,
+                 keepalive_timeout_interval: int = 60):
+        self.id = id
+        self.queue_id = ipfs_queue_id
+        self.peer_id = None
+        self.ipfs = aioipfs_instance
+        self.our_ipfs_peer_id = None
+        self.tx_input, self.tx_output = Pipe()
+        self.rx_input, self.rx_output = Pipe()
+        self.status = PeeringStatus.IDLE
+        self.keepalive_send_interval = keepalive_send_interval
+        self.keepalive_timeout_interval = keepalive_timeout_interval
+        self.timeout = None
+        super().__init__()
+        self.logger = logging.getLogger(f"<IPRPCChannel:{self.queue_id}>")
+
+    def run(self) -> None:
+        asyncio.ensure_future(self._handle_establish_connection())
+        asyncio.ensure_future(self._handle_incoming_messages())
+        asyncio.ensure_future(self._handle_keepalive())
+        asyncio.ensure_future(self._handle_timeout())
+        loop = asyncio.get_event_loop()
+        loop.run_forever()
+
+    def _change_peering_status(self, new_status: PeeringStatus):
+        self.logger.info(f"Peering status change from {self.status} to "
+                         f"{new_status}")
+        self.status = new_status
+
+    async def _handle_establish_connection(self):
+        while True:
+            if not self.status == PeeringStatus.ESTABLISHED:
+                self._change_peering_status(PeeringStatus.ESTABLISHING)
+                await self._send_message(PeeringHello(initiator_id=self.id))
+            await asyncio.sleep(5)
+
+    async def _handle_timeout(self):
+        while True:
+            if time.time() > self.timeout:
+                if not self.status == PeeringStatus.ESTABLISHING:
+                    self._change_peering_status(PeeringStatus.IDLE)
+            await asyncio.sleep(5)
+
+    async def _handle_incoming_messages(self):
+        self.timeout = time.time() + self.keepalive_timeout_interval
+        while True:
+            async for rx_message in self._get_message():
+                if type(rx_message) is PeeringHello:
+                    await self._send_message(
+                        PeeringHelloResponse(responder_id=self.id)
+                    )
+                elif type(rx_message) is PeeringHelloResponse:
+                    self.peer_id = rx_message.responder_id
+                    self._change_peering_status(PeeringStatus.ESTABLISHED)
+                elif type(rx_message) is PeeringKeepalive:
+                    self.timeout = time.time() + \
+                                   self.keepalive_timeout_interval
+                else:
+                    self.logger.info(f"Would have sent {rx_message} to queue")
+
+    async def _handle_keepalive(self):
+        keepalive_send_timeout = time.time() + self.keepalive_send_interval
+        while True:
+            if self.status == PeeringStatus.ESTABLISHED:
+                if time.time() > keepalive_send_timeout:
+                    await self._send_message(PeeringKeepalive())
+                    keepalive_send_timeout = time.time() + \
+                        self.keepalive_send_interval
+            await asyncio.sleep(5)
+
+    async def _set_our_ipfs_peer_id(self) -> None:
+        """This sets self.our_ipfs_peer_id so we can ignore messages we sent"""
+        id_info = await self.ipfs.core.id()
+        self.our_ipfs_peer_id = id_info.get('ID')
+
+    async def _send_message(self, call: IPRPCMessage):
+        await self._send_ipfs(call.serialize_to_json())
+        self.logger.info(f"Sent message: {call}")
+
+    async def _send_ipfs(self, message: str) -> None:
+        async with self.ipfs as ipfs:
+            await ipfs.pubsub.pub(self.queue_id, message)
+        self.logger.info(f"Sent raw message: {message}")
+
+    async def _get_from_ipfs(self):
+        if not self.our_ipfs_peer_id:
+            await self._set_our_ipfs_peer_id()
+            self.logger.info(f"Set our peer id to {self.our_ipfs_peer_id}")
+        else:
+            async with self.ipfs as ipfs:
+                async for message in ipfs.pubsub.sub(self.queue_id):
+                    if not message['from'].decode() == self.our_ipfs_peer_id:
+                        raw_message = unquote(message['data'].decode('utf-8'))
+                        self.logger.info(f"Got raw message: {message}")
+                        yield raw_message
+
+    async def _get_message(self):
+        async for message in self._get_from_ipfs():
+            try:
+                yield IPRPCRegistry.deserialize_from_json(message)
+            except Exception as e:
+                self.logger.warning(f"Decoding failed on message: {e}")
+
+    async def _close_aio_connection(self):
+        await self.ipfs.close()
+
+    def __del__(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self._close_aio_connection())
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}:queue_id={self.queue_id}," \
+            f"peer_id={self.peer_id}," \
+            f"status={self.status.name}>"
 
 
 class CallChannel:
@@ -51,20 +177,14 @@ class CallChannel:
         return f"<CallChannel: queue_id={self.queue_id}>"
 
 
-class PeeringStatus(Enum):
-    IDLE = 1
-    ESTABLISHING = 2
-    ESTABLISHED = 3
-
-
 class PeerChannel:
 
     def __init__(self,
                  peer_id: str,
                  rx_queue_id: str,
                  tx_queue_id: str,
-                 keepalive_interval: int = 10,
-                 keepalive_timeout: int = 20):
+                 keepalive_interval: int = 60,
+                 keepalive_timeout: int = 120):
         self.our_id = peer_id
         self.peer_id = None
         self.ready = False
--- pillar/IPRPC/tests/test_channel.py
+++ pillar/IPRPC/tests/test_channel.py
@@ -1,10 +1,14 @@
 from unittest import TestCase, SkipTest
-from ..channel import CallChannel, PeerChannel
+from ..channel import CallChannel, PeerChannel, IPRPCChannel
+from ..messages import PeeringHello
 from unittest.mock import MagicMock
 import aioipfs
 import asyncio
 import asynctest
+import logging
 from multiprocessing import Pipe
+
+logging.basicConfig(level=logging.INFO)
 
 
 class AsyncMock(MagicMock):
@@ -76,3 +80,60 @@ class TestPeerChannel(TestCase):
         self.assertEqual(peer_id, pc.our_id)
         self.assertEqual(tx_queue_id, pc.tx_queue_id)
         self.assertEqual(rx_queue_id, pc.rx_queue_id)
+
+    def test_peerchannel_peering(self):
+        pc1 = PeerChannel("peer_1", "rx_queue", "tx_queue")
+        pc2 = PeerChannel("peer_2", "tx_queue", "rx_queue")
+        # Using queues to mock connection between PeerChannels
+        pc1_tx, pc2_rx = Pipe()
+        pc2_tx, pc1_rx = Pipe()
+        pc1.tx_pipe = pc1_tx
+        pc1.rx_pipe = pc1_rx
+        pc2.tx_pipe = pc2_tx
+        pc2.rx_pipe = pc2_rx
+
+
+class TestIPRPCChannel(TestCase):
+
+    def setUp(self) -> None:
+        ipfs_instance = aioipfs.AsyncIPFS()
+        ipfs_instance.pubsub = AsyncMock()
+        self.channel = IPRPCChannel('test_id',
+                                    'testing_queue',
+                                    ipfs_instance)
+
+    def test_channel_creation(self):
+        self.assertEqual('testing_queue', self.channel.queue_id)
+
+    def test_send_message(self):
+        test_string = "Hello, sending test message!"
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.channel._send_ipfs(test_string))
+        with self.assertLogs(level='INFO') as cm:
+            logging.getLogger("<IPRPCChannel:testing_queue>")\
+                .info('Sent raw message: Hello, sending test message!')
+        self.assertEqual(cm.output, ['INFO:<IPRPCChannel:testing_queue>:'
+                                     'Sent raw message: Hello, sending'
+                                     ' test message!'])
+        self.channel.ipfs.pubsub.pub.assert_called_with('testing_queue',
+                                                        test_string)
+
+    def test_channel_repr(self):
+        repr_string = self.channel.__repr__()
+        expected = '<IPRPCChannel:queue_id=testing_queue,peer_id=None,' \
+                   'status=IDLE>'
+        self.assertEqual(expected, repr_string)
+
+    def test_send_call(self):
+        test_call = PeeringHello(initiator_id="hi")
+        loop = asyncio.get_event_loop()
+        self.channel._send_ipfs = AsyncMock()
+        loop.run_until_complete(self.channel._send_message(test_call))
+        self.channel._send_ipfs.assert_called_with(test_call.
+                                                   serialize_to_json())
+
+    def test_channel_own_peer_id_set(self):
+        self.channel.ipfs.core.id = AsyncMock(return_value={"ID": "test_id"})
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.channel._set_our_ipfs_peer_id())
+        self.assertEqual('test_id', self.channel.our_ipfs_peer_id)
