6d726eb92711285de63b6501a61312cf162ef1c6
Spencer Harmon <spencerharmon.91@gmail.com>
2021-01-03 12:12:37 -0600

    don't make dirs.

--- pillar/tests/test_keymanager.py
+++ pillar/tests/test_keymanager.py
@@ -10,7 +10,6 @@ class TestKeyManager(TestCase):
                                   'data/config.yaml')
 
         self.config = Config(path=configpath)
-        os.makedirs(os.path.abspath(self.config.gpghome), exist_ok=True)
 
     def test_instantiate_keymanager_class(self):
         km = KeyManager(self.config)
