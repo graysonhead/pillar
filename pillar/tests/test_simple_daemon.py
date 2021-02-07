97c5e2eaefed611038a72077b5edcdf372c6967c
Spencer Harmon <spencerharmon.91@gmail.com>
2021-02-07 17:36:35 -0600

    test simple daemon's prerun

--- pillar/tests/test_simple_daemon.py
+++ pillar/tests/test_simple_daemon.py
@@ -1,9 +1,22 @@
 from pillar.config import PillardConfig
 from pillar.simple_daemon import Daemon
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
 
 
 class TestSimpleDaemon(TestCase):
     def test_instantiate(self):
         config = PillardConfig(config_directory="/this/shouldnt/exist")
         Daemon(config)
+
+
+class MoreTestSimpleDaemon(TestCase):
+    def setUp(self):
+        self.config = PillardConfig(config_directory="/this/shouldnt/exist")
+        self.daemon = Daemon(self.config)
+
+    @patch('multiprocessing.Process.run', new_callable=MagicMock)
+    @patch('pillar.multiproc.PillarWorkerThread.run', new_callable=MagicMock)
+    def test_pre_run(self, *args):
+        self.daemon.pre_run()
+        assert(True)
