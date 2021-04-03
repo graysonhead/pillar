fbb6d3e547c297bd4cdcdcb71e8bcd9e7ba2a5fc
Spencer Harmon <spencerharmon.91@gmail.com>
2021-04-03 15:25:02 -0500

    clean up unneeded code comment

--- pillar/identity.py
+++ pillar/identity.py
@@ -63,10 +63,8 @@ class LocalIdentity(PillarDBObject,
             get_and_decrypt_message_from_cid(cid, verify=False)
         if not type(invitation) is InvitationMessage:
             raise WrongMessageType(type(invitation))
-        peer_fingerprint = self.id_interface.key_manager.\
+        self.id_interface.key_manager.\
             import_or_update_peer_key(invitation.public_key_cid)
-#        key = self.id_interface.key_manager.get_key_from_keyring(  # noqa
-#            peer_fingerprint)
 
     def create_invitation(self, peer_fingerprint_cid):
         fingerprint, pubkey_cid = self._get_info_from_fingerprint_cid(
